1757501c2f203bd743cca4ab960803adc4c5a5ea
Artur Konopka <artur.konopka.1994@gmail.com>
2021-03-27 17:29:33 +0000

    Changed DB to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_19_163044) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
